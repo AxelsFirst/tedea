d04f90a5395616408aa0e29bfa34819948a39b65
AxelsFirst <alex.gibala@protonmail.com>
2024-01-14 02:29:15 +0100

    feature: Added Sidebar class scheme Sidebar object will be used to organize packing additional frames on the sidebar.

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import ttkbootstrap as tb
 
+
 class Main_Window(tb.Window):
     def __init__(self, themename='cerculean'):
         tb.Window.__init__(self, themename=themename)
@@ -18,3 +19,48 @@ class Main_Window(tb.Window):
 
         self.Main_Frame = Main_Frame(self, fig)
         self.Main_Frame.pack(fill='both', expand=True, padx=10, pady=10)
+
+
+class Sidebar(tb.Frame):
+    def __init__(self, root, bootstyle='light'):
+        tb.Frame.__init__(self, root, bootstyle=bootstyle)
+        self.Main_Window = root
+
+        self.side_label = tb.Label(self, 
+                                   text='Simplicial Complex Settings', 
+                                   bootstyle='inverse-light')
+        self.side_label.pack(pady=20, padx=20)
+
+        self.pack_separator()
+
+        self.Dimension_Frame = Dimension_Frame(self)
+        self.Dimension_Frame.pack()
+
+        self.pack_separator()
+
+        self.Vertex_Addition_Frame = Vertex_Addition_Frame(self)
+        self.Vertex_Addition_Frame.pack()
+
+        self.pack_separator()
+
+        self.Vertex_List_Frame = Vertex_List_Frame(self)
+        self.Vertex_List_Frame.pack()
+
+        self.pack_separator()
+
+        self.Metric_Frame = Metric_Frame(self)
+        self.Metric_Frame.pack()
+
+        self.pack_separator()
+
+        self.Plot_Config_Frame = Plot_Config_Frame(self)
+        self.Plot_Config_Frame.pack()
+
+        self.pack_separator()
+
+        self.Plot_Generation_Frame = Plot_Generation_Frame(self)
+        self.Plot_Generation_Frame.pack()
+
+    def pack_separator(self):
+        separator = tb.Separator(self, bootstyle='primary')
+        separator.pack(fill='x', padx=15, pady=25)
